72446207727dcd54fa8f58d36755310542b6cebe
Jayshol <jayshol@gmail.com>
2018-09-13 09:13:55 -0700

    initial commit

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Puzzle from './components/puzzle';
+import './index.css';
+
+
+ReactDOM.render(<Puzzle />, document.getElementById('root'));
+
